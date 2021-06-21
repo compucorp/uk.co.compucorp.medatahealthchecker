d89b236f90faacb525f7b26da15c8d93443b0acd
Omar Hussein <opetmar91@gmail.com>
2021-06-21 04:45:30 +0300

    BASW-733: Implement errors dashboard

--- CRM/Medatahealthchecker/Page/IssuesDashboard.php
+++ CRM/Medatahealthchecker/Page/IssuesDashboard.php
@@ -1,11 +1,26 @@
 <?php
-use CRM_Medatahealthchecker_ExtensionUtil as E;
+use CRM_Medatahealthchecker_DataChecker_ErrorCodes as ErrorCodes;
 
 class CRM_Medatahealthchecker_Page_IssuesDashboard extends CRM_Core_Page {
 
   public function run() {
-
+    $this->setIssuesForEachCategory();
     parent::run();
+  }
+
+  private function setIssuesForEachCategory() {
+    $issues = [];
+    $issuesCategories = ErrorCodes::getAllCodesWithDescription();
+    foreach ($issuesCategories as $errorCode => $description) {
+      $query = "SELECT count(*) FROM medatahealthchecker_issues_log WHERE error_code = {$errorCode}";
+      $issuesCount = CRM_Core_DAO::singleValueQuery($query);
+
+      if (!empty($issuesCount)) {
+        $issues[] = ['issues_count' => $issuesCount, 'description' => $description, 'error_code' => $errorCode];
+      }
+    }
+
+    $this->assign('issuesCategories', $issues);
   }
 
 }
---
+++ templates/CRM/Medatahealthchecker/Page/IssuesDashboard.tpl
@@ -0,0 +1,13 @@
+<div><b style="color:red;">These issues might require your attention:</b></div><br>
+
+{foreach from=$issuesCategories item=item key=n}
+  <div class="issues-category">
+    <h3 class="crm-severity-warning">
+      <i class="crm-i fa-exclamation-triangle" aria-hidden="true"></i>
+      [{$item.issues_count} record(s)] => {$item.description}
+      <div class="css_right">
+        <a href="civicrm/medatahealthchecker/search-error?error_code={$item.error_code}">View all</a>
+      </div>
+    </h3>
+  </div>
+{/foreach}
